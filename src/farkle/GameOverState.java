e37d82b5de68b8f71b2702e96c632700768f6a62
KnightRaven0 <firstwinter1@gmail.com>
2018-09-15 22:17:24 -0600

    FIXED GAME OVER STATE

--- src/farkle/GameOverState.java
+++ src/farkle/GameOverState.java
@@ -18,13 +18,14 @@ public class GameOverState extends GameState {
 	 */
 	@Override
 	public void draw(Graphics g) {
+		g.setColor(Color.RED);
         g.setFont(new Font("TimesRoman", Font.BOLD, 30));
 		int width = g.getFontMetrics().stringWidth("Winning with " + winner.getScore() + " points:");
 		g.drawString(winner.getName(), Renderer.WindowWidth - width / 2, 50);
         g.setFont(new Font("TimesRoman", Font.BOLD, 150));
 		width = g.getFontMetrics().stringWidth(winner.getName());
 		int height = g.getFontMetrics().getHeight();
-		g.drawString(winner.getName(), Renderer.WindowWidth - width / 2, Renderer.WindowHeight - height / 2);
+		g.drawString(winner.getName(), Renderer.WindowWidth/ 2 - width / 2, Renderer.WindowHeight / 2 - height / 2);
 	}
 	/**
 	 * Checks for the player to exit the game
