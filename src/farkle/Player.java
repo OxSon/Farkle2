1f62676548bccb0afc4c987dafac650a33fc773d
A-L-M <al3cmills@gmail.com>
2018-09-15 21:56:13 -0600

    comments and javadoc

--- src/farkle/Player.java
+++ src/farkle/Player.java
@@ -4,18 +4,31 @@ public class Player {
 
     private final String name;
     private int score;
+    //number of farkles
     private int farkles;
+    //computer-controlled or not
     private boolean isAI;
 
+    /**
+     * @param name
+     */
     public Player(String name) {
         this.name = name;
     }
 
+    /**
+     * @param name
+     * @param isAI computer controlled or not
+     */
     public Player(String name, boolean isAI) {
         this.name = name;
         this.isAI = isAI;
     }
 
+    /**
+     * handles internal score tracking at end of turn
+     * @param pointsScored
+     */
     public void finishTurn(int pointsScored) {
         if (pointsScored == 0) {
             farkles++;
